4b7645b059a703a2ad3ca1ce1e8f858163666dbe
Maria <araujomaria84@gmail.com>
2023-03-21 17:16:34 +0100

    Add react-starter-kit

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+/* SECCIÓN DE IMPORT */
+
+// - De React
+// - Nuestros
+// - Sass
+import '../styles/App.scss';
+// - Imágenes
+
+/* SECCIÓN DEL COMPONENTE */
+function App() {
+  /* VARIABLES ESTADO (DATOS) */
+
+  /* EFECTOS (día 5) */
+
+  /* FUNCIONES HANDLER */
+
+  /* FUNCIONES Y VARIABLES AUXILIARES PARA PINTAR EL HTML */
+
+  /* HTML */
+  return <div className="App">Hola Harry</div>;
+}
+
+/* PROP-TYPES */
+
+/* EXPORT DEL COMPONENTE */
+export default App;
