1deb2f4ba20ded096b1b9d806548051d637a7d05
Maria <araujomaria84@gmail.com>
2023-03-23 10:05:27 +0100

    Add scss CharacterDetail mobile version

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,32 +1,43 @@
 import { Link } from "react-router-dom";
 import images from './Images.js'
+import '../styles/layout/CharacterDetail.scss';
 
 function CharacterDetail ({characterFind}) {
     return(
         <>
-        <h2 className='detail title'>Detalles del personaje</h2>
+            <h2 className='detail__title'>Detalles del personaje</h2>
 
-            <section className='detail__page'>
+                <section className='detail__card'>
                     <img
-                    className='detail__img'
-                    src={characterFind.image !== '' ? characterFind.image : images[characterFind.house]}
-                    alt= {`Imagen de ${characterFind.name}`}
-                    title= {`Imagen de ${characterFind.name}`}
+                        className='detail__img'
+                        src={characterFind.image !== '' ? characterFind.image : images[characterFind.house]}
+                        alt= {`Imagen de ${characterFind.name}`}
+                        title= {`Imagen de ${characterFind.name}`}
                     />
-                    <h3 className='detail name'>Nombre</h3>
-                    <p className='text'>{characterFind.name}</p>
-                    <h3 className='detail house'>Casa</h3>
-                    <img src={images[characterFind.house]}></img>
-                    <h3 className='detail status'>Estado</h3>
-                    <p className='text'>{characterFind.alive ? 'Viv@' : 'Muert@' }</p>
-                    <h3 className='detail gender'>Género</h3>
-                    <p className='text'>{characterFind.gender}</p>
-                    <h3 className='detail specie'>Especie</h3>
-                    <p className='text'>{characterFind.specie}</p>
-                    <h3 className='detail othersnames'>Nombres alternativos</h3>
-                    <p className='text'>{characterFind.alternatename}</p>
-            </section>
-            <Link className="buttons" to='/'>Volver</Link>
+
+                    <article className="article">
+                        <h3 className='detail__desc'>Nombre: </h3>
+                        <p className='text'>{characterFind.name}</p>
+
+                        <h3 className='detail__desc'>Casa:</h3>
+                        <img className="detail__shield" src={images[characterFind.house]}></img>
+
+                        <h3 className='detail__desc'>Estado:</h3>
+                        <p className='text'>{characterFind.alive ? '🤍' : '💀' }</p>
+
+
+                        <h3 className='detail__desc'>Género:</h3>
+                        <p className='text_gender'>{characterFind.gender === 'female' ? '♀️'  : '♂️' }</p>
+
+
+                        <h3 className='detail__desc'>Especie:</h3>
+                        <p className='text'>{characterFind.specie}</p>
+
+                        <h3 className='detail__desc'>Nombres alternativos:</h3>
+                        <p className='text'>{characterFind.alternatename}</p>
+                    </article>
+                </section>
+            <Link className="back" to='/'>Volver</Link>
         </>
     );
 }
--- src/components/Images.js
+++ src/components/Images.js
@@ -2,11 +2,19 @@ import Gryffindor from '../../src/images/Gryffindor.png';
 import Hufflepuff from '../../src/images/Hufflepuff.png';
 import Ravenclaw from '../../src/images/Ravenclaw.png';
 import Slytherin from '../../src/images/Slytherin.png';
+import gGryffindor from '../../src/images/gorro_Gryffindor.jpg';
+import gHufflepuf from '../../src/images/gorro_Hufflepuff.jpg';
+import gRawenclaw from '../../src/images/gorro_Rawenclaw.jpeg';
+import gSlytherin from '../../src/images/gorro_Slytherin.jpg';
 
 
 export default {
     Gryffindor,
     Hufflepuff,
     Slytherin,
-    Ravenclaw
+    Ravenclaw,
+    gGryffindor,
+    gHufflepuf,
+    gRawenclaw,
+    gSlytherin,
 }
